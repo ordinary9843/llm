15fd4ef5fa8174dd57956e7739eb9b2f9755209a
Jerry Chen <jerry.chen@redso.com.hk>
2025-06-11 12:35:33 +0800

    chore(pip): update env requirements

--- src/config.py
+++ src/config.py
@@ -4,4 +4,5 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     model: str = "Qwen2-1.5B-Instruct"
+    # model: str = "Mistral-7B-Instruct-v0.3"
     endpoint: str = "http://127.0.0.1:8000"
